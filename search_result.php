01b3eb42f556fc67eda2706a83896fcedfe53137
LucasJurgec <105923018@student.swin.edu.au>
2025-05-15 15:38:37 +1000

    basic tags

--- search_result.php
+++ search_result.php
@@ -1,30 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Search Rseult</title>
+</head>
+<body>
 <?php
-require_once("settings.php");
+    require_once("settings.php");
 
-if (isset($_GET['model'])) {
-    $model = mysqli_real_escape_string($conn, $_GET['model']);
-    $sql = "SELECT * FROM cars WHERE model LIKE '%$model%'";
-    $result = mysqli_query($conn, $sql);
+    if (isset($_GET['model'])) {
+        $model = mysqli_real_escape_string($conn, $_GET['model']);
+        $sql = "SELECT * FROM cars WHERE model LIKE '%$model%'";
+        $result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) > 0) {
-        echo "<table border='1' cellpadding='5'>";
-        echo "<tr><th>ID</th><th>Make</th><th>Model</th><th>Price</th><th>Year</th></tr>";
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . $row['car_id'] . "</td>";
-            echo "<td>" . $row['make'] . "</td>";
-            echo "<td>" . $row['model'] . "</td>";
-            echo "<td>" . $row['price'] . "</td>";
-            echo "<td>" . $row['yom'] . "</td>";
-            echo "</tr>";
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table border='1' cellpadding='5'>";
+            echo "<tr><th>ID</th><th>Make</th><th>Model</th><th>Price</th><th>Year</th></tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['car_id'] . "</td>";
+                echo "<td>" . $row['make'] . "</td>";
+                echo "<td>" . $row['model'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['yom'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "🚫 No matching cars found.";
         }
-        echo "</table>";
     } else {
-        echo "🚫 No matching cars found.";
+        echo "Please enter a model to search.";
     }
-} else {
-    echo "Please enter a model to search.";
-}
 
-mysqli_close($conn);
-?>
+    mysqli_close($conn);
+    ?>
+</body>
+</html>
