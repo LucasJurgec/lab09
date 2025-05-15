6651157182482bf8ff37d1530d1d4433a73bbd0d
LucasJurgec <105923018@student.swin.edu.au>
2025-05-15 15:30:51 +1000

    exhibition_db fix

--- settings.php
+++ settings.php
@@ -2,5 +2,5 @@
     $host = "localhost";         // because XAMPP runs the server locally
     $user = "root";          // default username for XAMPP's MySQL
     $pwd = "";              // default password is empty in XAMPP
-    $sql_db = "exhibtion_db";  // replace with the actual name of your database
+    $sql_db = "exhibition_db";  // replace with the actual name of your database
 ?>
